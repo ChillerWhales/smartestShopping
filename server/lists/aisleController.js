e73460e1442eba913e1056bd8d94cd22d08b08f0
Monica Engel <monicaengel9@gmail.com>
2015-06-12 17:04:28 -0700

    [refactor] WIP

--- server/lists/aisleController.js
+++ server/lists/aisleController.js
@@ -1,13 +1,20 @@
 var request = require('request');
 var parseXml = require('xml2js').parseString;
-var config = require('./config')
+var config = {
+   usdaKey : 'mGJLNkyEOxqIQg0i99gC6by18qabPPIJbxc91w8H',
+   superMarketApiKey: 'd53f07be7d',
+   sampleSuperMarketId: 'e6k3fjw75k',
+};
 
 module.exports = function(req,res) {
-	var itemName = req.param('name');
-	console.log("req.params:", req.params);
-	console.log("req.param:", req.param);
+	var itemName = req.query.name;
+  console.log(itemName);
+  console.log(config.sampleSuperMarketId);
+  console.log(config.superMarketApiKey);
 	var url = "http://www.SupermarketAPI.com/api.asmx/SearchForItem?APIKEY=" + config.superMarketApiKey 
     + "&StoreId=" + config.sampleSuperMarketId + "&ItemName=" + itemName;
+    url = url.split(' ').join('+');
+    console.log("requested: ", url);
 
   request.get(url, function(err, requestRes, body) {
   	if (err) {
@@ -17,7 +24,8 @@ module.exports = function(req,res) {
   		if (err) {
   			console.error(err);
   		}
-      console.log(result.ArrayOfProduct.Product);
+      console.log(result);
+      console.log(result.ArrayOfProduct.Product); 
   		res.status(200).json(result.ArrayOfProduct.Product);
   	})
   })
